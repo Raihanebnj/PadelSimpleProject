065b49978cdef6d6f6eaff520fdae6b0727edeba
RaihaneBenjilali <raihane.benjilali@student.odisee.be>
2026-08-19 16:01:43 +0200

    maui

--- PadelSimple.Mobile/App.xaml.cs
+++ PadelSimple.Mobile/App.xaml.cs
@@ -1,10 +1,18 @@
-﻿namespace PadelSimple.Mobile;
+namespace PadelSimple.Mobile;
 
 public partial class App : Application
 {
+    private readonly AppShell _shell;
+
     public App(AppShell shell)
     {
         InitializeComponent();
-        MainPage = shell;
+        _shell = shell;
+    }
+
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        return new Window(_shell);
     }
 }
+
